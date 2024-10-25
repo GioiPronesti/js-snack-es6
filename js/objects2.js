f6e0f00f2add61e10a5bd355c790d7c777f77c5b
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-25 19:10:29 +0200

    es 2 object array teams random score

--- js/objects2.js
+++ js/objects2.js
@@ -3,4 +3,60 @@ Creare un array di oggetti di squadre di calcio.
  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+risultato = nuovo array di oggetti conteneti solo nomi e falli subiti 
+newarray = [ 
+    {
+        name: squadra
+        falli: randomnumber
+    },
+    {
+        name: squadra
+        falli: randomnumber
+
+    },
+    {
+
+        name: squadra
+        falli: randomnumber
+    }
+
+]
+
+*/
+//Creare un array di oggetti di squadre di calcio.
+//Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+const teams = [
+  {
+    name: "Parma",
+    score: 0,
+    fouls_taken: 0,
+  },
+  {
+    name: "Sassuolo",
+    score: 0,
+    fouls_taken: 0,
+  },
+  {
+    name: "Lecce",
+    score: 0,
+    fouls_taken: 0,
+  },
+];
+
+// genero numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+/*
+1 genero numeri rando sulla variabile random
+2 aggiorno il valore chiave score e fouls_taken da 0 a random number
+
+*/
+
+// 1 genero un numero random (1-10) lo assegno alla variabile randomNumber
+const minValue = 2;
+const maxValue = 100;
+
+let randomNunber = Math.floor(Math.random() * (maxValue - minValue) + minValue);
+console.log(randomNunber);

07ba7c36a69f033dde87886c6eae7a7cbd044d8b
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-25 16:14:16 +0200

    es 1 objects array

--- js/objects1.js
+++ js/objects1.js
@@ -3,3 +3,22 @@ Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
+
+console.log("hello console");
+
+const bikes = [
+  {
+    name: "flash",
+    weight: "30kg",
+  },
+  {
+    name: "lightning",
+    weight: "20kg",
+  },
+  {
+    name: "lightning",
+    weight: "45kg",
+  },
+];
+
+console.log("la bici più leggera ha un peso di", bikes[1].weight);

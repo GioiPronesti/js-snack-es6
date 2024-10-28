0c125ab3e0895af1dc79ac73951d741357dbac30
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-28 13:37:34 +0100

    es 3 object array range values

--- js/objects3.js
+++ js/objects3.js
@@ -3,3 +3,74 @@ Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 */
+
+/*
+controlli array elementi deve essere un array
+valore di start, e end deveno essere dei numeri interi
+start deve essere minore o uguale a end 
+end deve essere min o uguale a element.length
+
+creo un funzione che mi prende come parametro ( element array , start end )
+
+mi ritorna un nuovo array con dei valori in quella posizione tra start e end 
+*/
+
+/**
+ *
+ * questa funxiona prende un array e ritorna ..
+ * @param {Array} elements
+ * @param {Number} start
+ * @param {Number} end
+ * @returns {Array | undefined}
+ */
+function sliceArray(elements, start, end) {
+  /**
+   * Controlli:
+   * elements must be an array
+   * start, end must be a number (integer)
+   */
+  if (!Array.isArray(elements)) {
+    console.error("Elements must be array");
+    return;
+  }
+
+  if (isNaN(start)) {
+    console.error("Start is not a number");
+    return;
+  }
+
+  if (isNaN(end)) {
+    console.error("End is not a number");
+    return;
+  }
+
+  // start >= 0
+  start = Math.max(start, 0);
+  // if (start < 0) {
+  // 	start = 0
+  // }
+
+  // end <= elements.length
+  end = Math.min(end, elements.length);
+  // if (end > elements.length) {
+  // 	end = elements.length
+  // }
+
+  const result = [];
+
+  for (let i = start; i < end; i++) {
+    const el = elements[i];
+    // console.log(i, el)
+
+    result.push(el);
+  }
+
+  return result;
+}
+
+const numbers = [4, 6, 5, 9, 7, 8, 6, 1, 2];
+console.log(numbers);
+// numbers.slice(3,6)  // [9,7,8]
+
+const newNumbers = sliceArray(numbers, 2, 4);
+console.log(newNumbers); // [5,9]
